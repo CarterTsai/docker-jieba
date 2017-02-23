0ec02f8012c103a572d0356870425480958a53b3
Carter Tsai <hamming1@gmail.com>
2017-02-23 23:54:22 +0800

    add node 6.10.0-alpine to mini image size  Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Jieba Docker TEST
 
 $> docker build docker/ -t carter/jieba
 
-$> docker run -it -v "$PWD/code:/nodejs/code"  carter/jieba
+$> docker run -it -v "$PWD/code:/nodejs/code" -p "3000:3000"  carter/jieba
+
+$> docker ps  -a | grep Exited | awk -F' ' '{print $1}' | xargs docker rm
+
+$> docker images | grep none | awk -F' ' '{print $3}'| xargs docker rmi
--- code/index.js
+++ code/index.js
@@ -1,3 +1,13 @@
 var nodejieba = require("nodejieba");
-var result = nodejieba.cut("南京市长江大桥");
-console.log(result);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  var result = nodejieba.cut("南京市长江大桥");
+  res.send(result);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
